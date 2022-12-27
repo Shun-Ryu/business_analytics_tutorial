7bb72c06609b2e59f635bc979853a31a8cb350ff
Shun Ryu <seunghun_yu@korea.ac.kr>
2022-12-27 11:28:21 +0900

    == Added MixMatch Tutorial Code ==
    * Notebook
    * Wide-ResNet
    * Utility Functions

---
+++ 5_semi-supervised_learning/Tutorials/utils/misc.py
@@ -0,0 +1,31 @@
+import errno
+import os
+
+
+__all__ = ['mkdir_p', 'AverageMeter']
+
+def mkdir_p(path):
+    '''make dir if not exist'''
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
